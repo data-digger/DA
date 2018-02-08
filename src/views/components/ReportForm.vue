4e5f310a300e7720d7be1fb0e3fd75426d2f9e3b
caianqi <763367640@qq.com>
2018-02-08 11:16:41 +0800

    报表

--- src/views/components/ReportForm.vue
+++ src/views/components/ReportForm.vue
@@ -3,7 +3,7 @@
       <grid-layout :layout="report.defineJSON.content.portlets":col-num="12":row-height="30":is-draggable="false":is-resizable="false":vertical-compact="true":use-css-transforms="true">
             <grid-item v-for="item in report.defineJSON.content.portlets":x="item.x":y="item.y":w="item.w":h="item.h":i="item.i":key='item.i'>
               <div class='griditem-title'>{{item.i}}</div>
-              <div :id="'chartBox'+item.i" style='height:90%;' v-if='show'></div>                
+              <div :id="report.id+item.i" style='height:90%;' ></div>                
             </grid-item>
          </grid-layout>
     </div>    
@@ -22,7 +22,7 @@ export default {
   },
   data(){
     return {
-       show:false,
+     
     }
   }, 
   methods:{
@@ -31,27 +31,19 @@ export default {
             Vue.AxiosPost("getReportData",{'reportID':Vue.report.id},
             function(response){
               console.log(response)
-              // var portlets = JSON.parse(response.data.defineJSON).content.portlets;
-              // var chartData = response.data.data;              
-              // for(var i in portlets){
-              //   var portletID = portlets[i].portletID;
-              //   for(var j in portlets[i].tabs){
-              //      var chartID = portlets[i].tabs[j].objid;
-            //        var gridData = chartData[chartID].gridData;
-            //        var chartDefineJSON = chartData[chartID].defineJSON;
-            //        var type = chartData[chartID].type;
-            //        console.log(chartDefineJSON);
-            //        var eoption = eval('(' + chartDefineJSON + ')');
-            //        chartUtil.analysis(eoption,type,gridData);
-            //        Vue.show = true;
-            //        Vue.$nextTick(function(){
-            //         // 基于准备好的dom，初始化echarts实例
-            //           let chartView = echarts.init(document.getElementById("chartBox"+portletID));
-            //        // 绘制图表
-            //           chartView.setOption(eoption);
-            //        })
-            //    }
-              // }
+               var portlets = JSON.parse(response.data.defineJSON).content.portlets;
+               var chartData = response.data.data;     
+               for (var i in chartData){
+                var cData = chartData[i];
+                var gridData = cData.gridData;
+                var chartDefineJSON = cData.defineJSON;
+                var type = cData.type;
+                console.log(chartDefineJSON);
+                var eoption = eval('(' + chartDefineJSON + ')');
+                chartUtil.analysis(eoption,type,gridData);
+                let chartView = echarts.init(document.getElementById(Vue.report.id +cData.portletID));
+                chartView.setOption(eoption);
+               }         
             })         
      }
   },
