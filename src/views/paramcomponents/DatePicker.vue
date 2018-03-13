861d2f06c9e1e56e11077f1126cc72e8ead7ed95
caianqi <763367640@qq.com>
2018-03-13 16:34:46 +0800

    查询器中日期控件添加默认日期

--- src/views/paramcomponents/DatePicker.vue
+++ src/views/paramcomponents/DatePicker.vue
@@ -1,10 +1,10 @@
 <template>
     <Row>
         <Col span="12">
-            <DatePicker v-if='monthShow' type="month" placeholder="Select month" style="width: 200px"@on-open-change='save' @on-change="handleChange"></DatePicker>
+            <DatePicker v-if='monthShow' type="month" placeholder="Select month" style="width: 200px"@on-open-change='save' @on-change="handleChange" :value='defaultDate'></DatePicker>
         </Col>
         <Col span="12">
-            <DatePicker v-if='DaterangeShow' type="date" :options="options1" placement="bottom-start" placeholder="Select date" style="width: 200px"  @on-open-change='save' @on-change="handleChange"></DatePicker>
+            <DatePicker v-if='DaterangeShow' type="date" :options="options1" placement="bottom-start" placeholder="Select date" style="width: 200px"  @on-open-change='save' @on-change="handleChange" :value='defaultDate'></DatePicker>
         </Col>
     </Row>
 </template>
@@ -13,7 +13,8 @@
         props:["dateType",'cmpContent'],
         data () {
             return {
-                dataValue:null,
+                selectedDate:null,
+                defaultDate:null,
                 monthShow:false,
                 DaterangeShow:false,
                 options1: {
@@ -55,12 +56,12 @@
         },
         methods:{
             handleChange(dateData){
-              this.dataValue = dateData;
+              this.selectedDate = dateData;
             },
             save(DatePicker){
                let Vue = this;
                if(DatePicker == false){
-                  Vue.$emit("sentDate",Vue.dataValue);
+                  Vue.$emit("sentDate",Vue.selectedDate);
                }
             }
         },
@@ -70,10 +71,12 @@
                if(Vue.cmpContent.paramType == 'date' && Vue.cmpContent.defaultDate.format == "yyyy-MM-dd"){
                   Vue.DaterangeShow = true;
                   Vue.monthShow = false;
+                  Vue.defaultDate = Vue.cmpContent.defaultDate.date;
                }
                if(Vue.cmpContent.paramType == 'date' && Vue.cmpContent.defaultDate.format == "yyyy-MM"){
                   Vue.monthShow = true;
                   Vue.DaterangeShow = false;
+                  Vue.defaultDate = Vue.cmpContent.defaultDate.date;
                }            
            }
            if(Vue.dateType){
