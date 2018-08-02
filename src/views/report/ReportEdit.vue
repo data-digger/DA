3c8f43da3660137e68c2d49779a5a1c91698d926
caianqi <763367640@qq.com>
2018-08-02 10:19:14 +0800

    修复添加模块

--- src/views/report/ReportEdit.vue
+++ src/views/report/ReportEdit.vue
@@ -83,10 +83,10 @@ export default {
   methods:{
     indexFilter(){
       let portlets = this.report.defineJSON.content.portlets;
-      let max = portlets[0].portletID;
+      let max = parseInt(portlets[0].portletID);
       for(let i in portlets){
         let portletID = portlets[i].portletID;
-        portletID>max ? max=portletID : null;
+        parseInt(portletID)>max ? max=parseInt(portletID) : null;
       }
       return parseInt(max);
     },
